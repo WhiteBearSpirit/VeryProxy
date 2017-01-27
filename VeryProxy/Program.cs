76bff920d773324c350132f9fa4e3ab71e8fd3d2
BotBand <BotBand@yandex.ru>
2017-01-27 10:02:02 +0300

    Legacy code removed.

--- VeryProxy/Program.cs
+++ VeryProxy/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -80,7 +77,7 @@ namespace VeryProxy
         }
 
 
-        static string ProcessURL_string(string url, ProxyManager proxx, int retryCount = 30)
+        static string ProcessURL_string(string url, ProxyManager proxx, int retryCount)
         {
             if (proxx == null) { throw new ArgumentNullException(); }
             for (int i = 0; i < retryCount; i++)
@@ -137,160 +134,5 @@ namespace VeryProxy
             Log.Line($"[Task failed after {retryCount} reties] ");
             return null;
         }
-
-        static Tuple<string, string> ProcessURL(string url, ProxyManager proxx, int retryCount = 30)
-        {
-            if (proxx == null) { throw new ArgumentNullException(); }
-            for (int i = 0; i < retryCount; i++)
-            {
-                //decreasing timeout up to half with every next retry
-                int myTimeout = (int)(Program.Timeout.TotalMilliseconds * (retryCount - (i / 2)) / retryCount);
-                string proxyString = proxx.GiveMeProxyString();
-                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-                try
-                {
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                    request.Proxy = new WebProxy(proxyString);
-                    request.Credentials = CredentialCache.DefaultCredentials;
-                    request.Timeout = myTimeout;
-                    request.ReadWriteTimeout = myTimeout;
-                    request.KeepAlive = false;
-                    sw.Start();
-                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                    {
-                        if (response.StatusCode == HttpStatusCode.OK)
-                        {
-                            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                            {
-                                string data = reader.ReadToEnd();
-                                reader.Close();
-                                response.Close();
-                                sw.Stop();
-                                double seconds = Math.Round(sw.Elapsed.TotalSeconds, 1);
-                                if (seconds < Timeout.TotalSeconds) { proxx.Report(proxyString, 2); }
-                                else { proxx.Report(proxyString, 1); }
-                                Log.SameLine("+");
-                                return Tuple.Create(url, data);
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    sw.Stop();
-                    int mark = (sw.Elapsed.TotalSeconds > (Timeout.TotalSeconds * 4)) ? -2 : -1;
-                    if (ex is WebException ||
-                        ex is System.IO.IOException)
-                    {
-                        proxx.Report(proxyString, mark);
-                        Log.SameLine("-");
-                    }
-                    else
-                    {
-                        Log.SameLine("?");
-                        Log.Data(ex);
-                    }
-                }
-            }
-            Log.Line($"[Task failed after {retryCount} reties] ");
-            return null;
-        }
-
-
-        static async Task<Tuple<string, string>> ProcessURLAsync_httpclient(string url, ProxyManager proxx, int retryCount = 30)
-        {
-            if (proxx == null) { throw new ArgumentNullException(); }
-
-            for (int i = 0; i < retryCount; i++)
-            {
-                string proxyString = proxx.GiveMeProxyString();
-                var handler = new HttpClientHandler
-                {
-                    UseDefaultCredentials = false,
-                    Proxy = new WebProxy(proxyString),
-                    UseProxy = true,
-                };
-                HttpClient myHttpClient = new HttpClient(handler) { Timeout = Timeout };
-                try
-                {
-                    var responseMessage = await myHttpClient.GetAsync(url);
-                    if (responseMessage.StatusCode == HttpStatusCode.OK)
-                    {
-                        string data = await responseMessage.Content.ReadAsStringAsync();
-                        proxx.Report(proxyString, 1);
-                        Log.SameLine("+");
-                        return Tuple.Create(url, data);
-                    }
-                    else
-                    {
-                        Log.SameLine($"[{(int)responseMessage.StatusCode}]");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (ex is System.Net.Http.HttpRequestException 
-                        || ex is WebException 
-                        || ex is TaskCanceledException
-                        || ex is System.IO.IOException)
-                    {
-                        proxx.Report(proxyString, -1);
-                        Log.SameLine("-");
-                    }
-                    else
-                    {
-                        Log.SameLine("?");
-                        Log.Data(ex);
-                    }
-                }
-            }
-            Log.Line($"[Task failed after {retryCount} reties] ");
-            return null;
-        }
-
-
-        static async Task<Tuple<string, string>> ProcessURLAsync_webclient(string url, ProxyManager proxx, int retryCount = 30)
-        {
-            if (proxx == null) { throw new ArgumentNullException(); }
-
-            for (int i = 0; i < retryCount; i++)
-            {
-                string proxyString = proxx.GiveMeProxyString();
-                var handler = new HttpClientHandler
-                {
-                    UseDefaultCredentials = false,
-                    Proxy = new WebProxy(proxyString),
-                    UseProxy = true,
-                };
-                WebClient client = new WebClient();
-                client.Proxy = new WebProxy(proxyString);
-                
-                try
-                {
-                    var response = await client.DownloadStringTaskAsync(url);
-                    proxx.Report(proxyString, 1);
-                    Log.SameLine("+");
-                    return Tuple.Create(url, response);
-                }
-                catch (Exception ex)
-                {
-                    if (ex is System.Net.Http.HttpRequestException
-                        || ex is WebException
-                        || ex is TaskCanceledException
-                        || ex is System.IO.IOException)
-                    {
-                        proxx.Report(proxyString, -1);
-                        Log.SameLine("-");
-                    }
-                    else
-                    {
-                        Log.SameLine("?");
-                        Log.Data(ex);
-                    }
-                }
-            }
-            Log.Line($"[Task failed after {retryCount} reties] ");
-            return null;
-        }
-
     }
 }
